11d2ed018ed4e2860994466cc96fd0e6b13bdd5c
Marina Flohr <marina.flohr@cantanea.com>
2018-04-18 09:49:48 +0300

    Watching for changes of @Input() in the child component

--- APM-Working/src/app/shared/filter/filter.component.ts
+++ APM-Working/src/app/shared/filter/filter.component.ts
@@ -1,12 +1,13 @@
-import { Component, OnInit, ViewChild, ElementRef, AfterViewInit, Input } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'pm-filter',
   templateUrl: './filter.component.html',
   styleUrls: ['./filter.component.css']
 })
-export class FilterComponent implements OnInit, AfterViewInit {
+export class FilterComponent implements OnInit, AfterViewInit, OnChanges {
   listFilter: string;
+  filterMsg: string;
   @Input() showDetails: boolean;
   @Input() foundItems: number;
   // access to the html reference- properies and methods
@@ -15,8 +16,16 @@ export class FilterComponent implements OnInit, AfterViewInit {
 
   ngOnInit() {
   }
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes);
+    if (changes['foundItems'] && !changes['foundItems'].currentValue) {
+      this.filterMsg = 'No hits';
+    } else {
+      this.filterMsg = 'Hits: ' + this.foundItems;
+    }
+  }
   ngAfterViewInit(): void {
-    console.log(this.filterElement);
+    // console.log(this.filterElement);
     // set focus
     this.filterElement.nativeElement.focus();
 
